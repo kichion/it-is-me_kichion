b8756ce4953163c4f43d8bcb43fb01868bfa8177
kichion <kkikuchi1539@gmail.com>
2023-01-13 19:47:43 +0900

    🎨 syntaxhighlight

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,8 +4,14 @@ import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import { buildClient, IPostFields } from "../lib/contentful";
 import { EntryCollection } from "contentful";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
+import {
+  documentToReactComponents,
+  Options,
+} from "@contentful/rich-text-react-renderer";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { ReactNode } from "react";
 const getPostEntries = async () => {
   const client = buildClient();
   const { items }: EntryCollection<IPostFields> = await client.getEntries({
@@ -41,6 +47,41 @@ const Post: NextPage<Props> = ({ posts }) => {
     return <ErrorPage statusCode={404} />;
   }
   const post = posts[0];
+  const Code = ({ children }: { children: ReactNode }) => (
+    <div>
+      <pre>
+        <code>{children}</code>
+      </pre>
+    </div>
+  );
+  const options: Options = {
+    renderNode: {
+      // コードブロックをdivで括る
+      [BLOCKS.PARAGRAPH]: (node, children) => {
+        console.info(node);
+        const n = node.content[0];
+        if (
+          node.content.length === 1 &&
+          n.nodeType === "text" &&
+          n.marks.find((x) => x.type === "code")
+        ) {
+          return <Code>{children}</Code>;
+        }
+        return <p>{children}</p>;
+      },
+    },
+    renderMark: {
+      [MARKS.CODE]: (node: ReactNode) => {
+        const texts = (node as string).split("\n");
+        console.info(node);
+        return (
+          <SyntaxHighlighter language={texts[0]} style={okaidia}>
+            {texts.slice(1)}
+          </SyntaxHighlighter>
+        );
+      },
+    },
+  };
   return (
     <div>
       <Head>
@@ -48,7 +89,7 @@ const Post: NextPage<Props> = ({ posts }) => {
       </Head>
       <main className="prose dark:prose-invert lg:prose-xl">
         <h1>{post.fields.title}</h1>
-        <div>{documentToReactComponents(post.fields.content)}</div>
+        <div>{documentToReactComponents(post.fields.content, options)}</div>
       </main>
     </div>
   );
